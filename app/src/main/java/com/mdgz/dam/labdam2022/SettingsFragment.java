158c863b3d0b670ebcf34625436b274d6651695a
MateoJereb <mateojereb@gmail.com>
2022-11-09 19:33:22 -0300

    XML preferencias terminado

---
+++ app/src/main/java/com/mdgz/dam/labdam2022/SettingsFragment.java
@@ -0,0 +1,64 @@
+package com.mdgz.dam.labdam2022;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragmentCompat;
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    private NavController navController;
+    private ListPreference metodoPagoList;
+    private ListPreference monedaList;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ActionBar actionBar = ((MainActivity) getActivity()).getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        navController = NavHostFragment.findNavController(this);
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        metodoPagoList = findPreference("metodo_pago");
+        monedaList = findPreference("moneda");
+
+        String metodoSelected = metodoPagoList.getValue();
+        monedaList.setEnabled(metodoPagoList.findIndexOfValue(metodoSelected) == 0);
+
+        metodoPagoList.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                int pos = metodoPagoList.findIndexOfValue(newValue.toString());
+                monedaList.setEnabled(pos == 0);
+
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey);
+    }
+}
